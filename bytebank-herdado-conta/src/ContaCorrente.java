b2a3b23fd51153112b59118e928256b5715396ec
Warton Barros <warton86@gmail.com>
2020-10-25 00:38:09 -0300

    Adicionando bytebank-herdado-conta

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,10 @@
+
+public class ContaCorrente extends Conta { // você herda atributos e métodos, mas não herda construtores
+	
+	public ContaCorrente(int agencia, int numero) { // construtor
+		
+		super(agencia, numero);
+		
+	}
+
+}
