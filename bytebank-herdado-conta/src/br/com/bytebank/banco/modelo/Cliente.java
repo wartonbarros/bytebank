854f03d336770b144cf39494b6bb8613356e6222
Warton Barros <warton86@gmail.com>
2020-10-28 20:19:30 -0300

    adicionando documentação

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Cliente.java
@@ -1,5 +1,12 @@
-package modelo;
+package br.com.bytebank.banco.modelo;
 
+
+
+/**
+ * Classe que representa um cliente no Bytebank.
+ * @author warton
+ * @version 0.1
+ */
 public class Cliente {
 
     private String nome;
